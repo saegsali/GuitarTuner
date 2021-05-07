535bada718fabeb53018e2453f2503645eb25d55
saegsali <joel@asper.ch>
2021-05-07 18:23:25 +0200

    comments

--- GuitarTuner code/src/main.cpp
+++ GuitarTuner code/src/main.cpp
@@ -1,5 +1,8 @@
+// Joel Asper, Marek Landert
+// 07.05.2021
+
 #include "arduinoFFT.h"
- 
+
 #define SAMPLES 128             //SAMPLES-pt FFT. Must be a base 2 number. Max 128 for Arduino Uno.
 #define SAMPLING_FREQUENCY 2048 //Ts = Based on Nyquist, must be 2 times the highest expected frequency.
 #define SCL_FREQUENCY 0x02
@@ -7,6 +10,7 @@
 const double Cor = 0.98;
 static const uint8_t NotenPins[] = {PD2, A5, A4, A3, A2, A1};
 
+// tone frequencies of an accoustic guitar
 const double FreqE2 = 82.41;
 const double FreqA2 = 110.00;
 const double FreqD3 = 146.83;
@@ -14,8 +18,7 @@ const double FreqG3 = 196.00;
 const double FreqH3 = 246.94;
 const double FreqE4 = 329.63;
 
-
-
+// peak correction factors
 const double LowerFac = 0.92;
 const double HigherFac = 1.08;
 
@@ -29,26 +32,26 @@ double vReal[SAMPLES]; //create vector of size SAMPLES to hold real values
 double vImag[SAMPLES]; //create vector of size SAMPLES to hold imaginary values
 
 void LEDaus(){
-  for(int i = 0; i<6; i++){
-    digitalWrite(NotenPins[i], LOW);
-  }
+	for(int i = 0; i<6; i++){
+    	digitalWrite(NotenPins[i], LOW);
+  	}
 }
 
 void PrintVector(double *vData, uint16_t bufferSize, uint8_t scaleType){
-      for (uint16_t i = 0; i < bufferSize; i++){
+	for (uint16_t i = 0; i < bufferSize; i++){
         double abscissa;
         abscissa = ((i * 1.0 * SAMPLING_FREQUENCY) / SAMPLES);
         Serial.print(abscissa, 2);
         if(scaleType==SCL_FREQUENCY)
-          Serial.print("Hz");
+        	Serial.print("Hz");
         Serial.print(" ");
         Serial.println(vData[i], 4);
-      }
-      Serial.println();
+    }
+    Serial.println();
 }
 
-void setup() 
-{
+void setup(){
+	// LED output pins
     pinMode(A5, OUTPUT);
     pinMode(A4, OUTPUT);
     pinMode(A3, OUTPUT);
@@ -60,16 +63,15 @@ void setup()
     pinMode(7, OUTPUT);
     pinMode(6, OUTPUT);
     pinMode(PD2, OUTPUT);
+
     Serial.begin(115200); //Baud rate for the Serial Monitor
     samplingPeriod = round(1000000*(1.0/SAMPLING_FREQUENCY)); //Period in microseconds 
     pinMode(A0, INPUT);
 }
  
-void loop() 
-{  
+void loop(){  
     /*Sample SAMPLES times*/
-    for(int i=0; i<SAMPLES; i++)
-    {
+    for(int i=0; i<SAMPLES; i++){
         microSeconds = micros();    //Returns the number of microseconds since the Arduino board began running the current script. 
      
         vReal[i] = analogRead(A0); //Reads the value from analog pin 0 (A0), quantize it and save it as a real term.
@@ -87,65 +89,61 @@ void loop()
     FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
     FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
 
-
-    
     /*Find peak frequency and print peak*/
     double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
-    if(peak > 0){
-      peak = peak*Cor;
-      //LEDs aus
-        
-      //E2 & E4
-      if(LowerFac*FreqE4 < peak && peak < HigherFac*FreqE4){
-        // LED aus
-        LEDaus();
-        digitalWrite(PD2, HIGH);
-        digitalWrite(A1, HIGH);
-      }
-      
-      // A2
-      else if(LowerFac*FreqA2 < peak && peak < HigherFac*FreqA2){
-        // LED aus
-        LEDaus();
-        digitalWrite(A5, HIGH);
-      }
-      
-      // D3
-      else if(LowerFac*FreqD3 < peak && peak < HigherFac*FreqD3){
-        // LED aus
-        LEDaus();
-        digitalWrite(A4, HIGH);
-      }
-      
-      // G3
-      else if(LowerFac*FreqG3 < peak && peak < HigherFac*FreqG3){
-        // LED aus
-        LEDaus();
-        digitalWrite(A3, HIGH);
-      }
-      
-      // H3
-      else if(LowerFac*FreqH3 < peak && peak < HigherFac*FreqH3){
-        // LED aus
-        LEDaus();
-        digitalWrite(A2, HIGH);
-      }
-      else{
-        LEDaus();
-      }
-      delay(50);
+    
+    if(peak > 5){ // minimize bullshit
+      	peak = peak*Cor; // Correct peak frequency
 
-      PrintVector(vReal, (SAMPLES >> 1), SCL_FREQUENCY);
-      delay(5000);
+		/* detect nearest string frequency */
+      	// E2 & E4
+      	if(LowerFac*FreqE4 < peak && peak < HigherFac*FreqE4){
+        	LEDaus();
+			// TODO: Fallunterscheidung hohes/tiefes E
+        	digitalWrite(PD2, HIGH);
+        	digitalWrite(A1, HIGH);
+      	}
       
-      Serial.println(peak);     //Print out the most dominant frequency.
-      
+		// A2
+		else if(LowerFac*FreqA2 < peak && peak < HigherFac*FreqA2){
+			LEDaus();
+			digitalWrite(A5, HIGH);
+		}
+		
+		// D3
+		else if(LowerFac*FreqD3 < peak && peak < HigherFac*FreqD3){
+			LEDaus();
+			digitalWrite(A4, HIGH);
+		}
+		
+		// G3
+		else if(LowerFac*FreqG3 < peak && peak < HigherFac*FreqG3){
+			LEDaus();
+			digitalWrite(A3, HIGH);
+		}
+		
+		// H3
+		else if(LowerFac*FreqH3 < peak && peak < HigherFac*FreqH3){
+			LEDaus();
+			digitalWrite(A2, HIGH);
+		}
+
+		// no clear frequency
+		else{
+			LEDaus();
+		}
+		delay(50);
+
+		/*
+		PrintVector(vReal, (SAMPLES >> 1), SCL_FREQUENCY);
+		delay(5000);
+		*/
+
+		Serial.println(peak);     //Print out the most dominant frequency.
+		
 
       
     }
-    
- 
-    /*Script stops here. Hardware reset required.*/
-    //while (1); //do one time
+
 }
 
